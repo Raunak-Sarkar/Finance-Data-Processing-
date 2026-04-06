cc9ae7ca8d478fb2df8ecf3882847d7c2b9f7ac9
Raunak Sarkar <raunaksarkar14@gmail.com>
2026-04-06 20:40:52 +0530

    add prisma migration

---
+++ prisma/migrations/20260406150350_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'VIEWER',
+    "status" TEXT NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "FinancialRecord" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "amount" DECIMAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "notes" TEXT,
+    "createdById" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME,
+    CONSTRAINT "FinancialRecord_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_date_idx" ON "FinancialRecord"("date");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_category_idx" ON "FinancialRecord"("category");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_type_idx" ON "FinancialRecord"("type");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_deletedAt_idx" ON "FinancialRecord"("deletedAt");
